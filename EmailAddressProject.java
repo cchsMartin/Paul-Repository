0e60a8f56218cd605ef57bb58943ace768f097c8
cchsMartin <54595803+cchsMartin@users.noreply.github.com>
2020-10-18 10:46:33 -0400

    Update EmailAddressProject.java

--- EmailAddressProject.java
+++ EmailAddressProject.java
@@ -8,6 +8,9 @@ public class EmailAddressProject {
 		System.out.println("Welcome to the Colgan Shark Mail Email Provider.");
 		System.out.println(" ");
 		
+		//If your variable contains two words, the first letter of the second word is capitalized
+		//For instance, firstName is correct Java style.
+		
 		System.out.println("Please enter your First Name.");// Asks first question
 		String firstname = keyboard.nextLine(); // Stores user input for First name
 		
@@ -31,6 +34,8 @@ public class EmailAddressProject {
 		System.out.println(" ");
 		
 		// converts inputs to correct punctuation
+		//you can update the variable to be lowercase. for instance middleName = middleName.toLowerCase() will overwrite the value with an all lowercase string
+		//these lines are redundant
 		String lowercaselast = lastname.toLowerCase();
 		String capitalfirst = firstname.toUpperCase();
 		String capitalmiddlei = middlename.toUpperCase();
@@ -45,7 +50,7 @@ public class EmailAddressProject {
 		String epassworddd = birthdate.substring(3, 5);
 		String epasswordyyyy = birthdate.substring(6, 10);
 		
-		
+		//you can concatenate the email address with the necessary part of each first, middle, and last name.
 		// prints a sentence that notifies user of generation of email and password
 		System.out.println(" ");
 		System.out.println("Your Email Address and Temporary Password have been generated.");
@@ -54,11 +59,14 @@ public class EmailAddressProject {
 		System.out.println("Email: " + /*lastnameemail*/lowercaselast + firstnameemail + middleinitemail + "@sharkmail.com");
 		System.out.println("");
 		
+		//you can also process the birthdate as one substring, instead of storing each part into a separate variable.
 		System.out.println("Temporary Password: " + epasswordmm + "" + epassworddd + "" + epasswordyyyy);
 		
 		System.out.println("NOTE: *** THIS PASSWORD IS TEMPORARY AND VERY INSECURE. PLEASE CHANGE PASSWORD AS SOON AS POSSIBLE! ***");
 		
 
 	}
-
 }
+
+//Your code works. And, that's the whole point. But, learn to make more efficient use of your code. Doing, so will greatly reduce time with debugging (because there are fewer variables)
+//100/100
